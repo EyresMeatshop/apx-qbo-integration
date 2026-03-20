ea888addcfac094d40edd3a3b40edc0a2ab8e71f
Daniel Gloumeau <eyresmeatshopoistins@gmail.com>
2026-03-20 13:49:22 -0400

    added hosted port

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def success():
     return "QuickBooks Connected Successfully"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.getenv("PORT", "10000"))
+    app.run(host="0.0.0.0", port=port)
